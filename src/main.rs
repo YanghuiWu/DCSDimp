dc8c47e8d5b1646a83d4c228365e635d0bcdc7de
woody <woodyy.wu@outlook.com>
2023-06-11 02:10:30 -0400

    Refine

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::io;
-use std::ops::DerefMut;
 use std::str::FromStr;
 
 use rand::distributions::Distribution;
 use rand::distributions::WeightedIndex;
-use rand::prelude::ThreadRng;
 
 /// Struct for weighted random sampling
 struct Generator {
-    random: RefCell<ThreadRng>,
     distribution: WeightedIndex<f64>,
     source: Vec<u64>,
 }
@@ -19,13 +15,11 @@ struct Generator {
 impl Generator {
     /// Create a new sampler from an iterator of (item, weight) tuples
     fn new<T: Iterator<Item=(u64, f64)>>(t: T) -> Generator {
-        let r = RefCell::new(rand::thread_rng());
         let vector: Vec<(u64, f64)> = t.into_iter().collect(); //Guarantees our index ordering.
         let distribution = WeightedIndex::new(vector.iter().map(|(_, weight)| *weight)).unwrap();
         let source = vector.into_iter().map(|(item, _)| item).collect();
 
         Generator {
-            random: r,
             distribution,
             source,
         }
@@ -33,9 +27,8 @@ impl Generator {
 
     /// Sample an item using the weighted distribution
     fn generate(&self) -> u64 {
-        let index = self
-            .distribution
-            .sample(self.random.borrow_mut().deref_mut());
+        let mut random = rand::thread_rng();
+        let index = self.distribution.sample(&mut random);
         self.source[index]
     }
 }
@@ -70,11 +63,6 @@ impl Simulator {
         self.step += 1;
         self.size -= self.tracker.remove(&self.step).unwrap_or(0);
     }
-
-
-    fn _get_size(&self) -> u64 {
-        self.size
-    }
 }
 
 /// Perform the caching simulation
@@ -83,17 +71,12 @@ fn caching(ten_dist: Generator, _cache_size: u64, _delta: f64, samples_to_issue:
     // let samples_to_issue: u64 = length as u64;
     // let mut prev_output: Vec<u64> = vec![0; samples_to_issue + 1];
     let mut dcsd_observed = vec![0; samples_to_issue + 1];
-    let mut time = 0;
-    //this part of code is for warmup cycles, but currently unused.
-    if time == 0 {
-        let tenancy = ten_dist.generate();
-        cache.add_tenancy(tenancy);
-        time += 1;
-    }
-
     let mut cycles = 0;
+    let tenancy = ten_dist.generate();
+    cache.add_tenancy(tenancy);
+
     loop {
-        println!("cycle: {}", cycles);
+        // println!("cycle: {}", cycles);
         if cycles > 100000 {//this is the main loop, larger numbers of loop gives higher precisions
             return dcsd_observed.clone();
         }
@@ -111,14 +94,9 @@ fn caching(ten_dist: Generator, _cache_size: u64, _delta: f64, samples_to_issue:
 
 fn get_sum(input: &Vec<u64>) -> u128 {
     let mut sum: u128 = 0;
-    let mut index: usize = 0;
-    for k in input {
-        sum += *k as u128;
-        if index == input.len() {
-            break;
-        }
-        index += 1;
-    }
+
+    input.iter().for_each(|&k| sum += k as u128);
+
     if sum == 0 {
         return 1;
     }
@@ -126,27 +104,22 @@ fn get_sum(input: &Vec<u64>) -> u128 {
 }
 
 
-fn input_to_hashmap() -> (HashMap<u64, f64>, usize) {
-    // let mut rdr = csv::ReaderBuilder::new().from_reader(io::stdin());
+fn input_to_hashmap() -> (Generator, usize) {
     let mut rdr = csv::Reader::from_reader(io::stdin());
-    let mut _result: HashMap<u64, f64> = HashMap::new();
+    let mut tenancy_dist: HashMap<u64, f64> = HashMap::new();
     let mut largest = 0;
     for result in rdr.records() {
         let record = result.unwrap();
-        // if record.get(0).unwrap().parse::<usize>().unwrap() > largest {
-        //     largest = record.get(0).unwrap().parse().unwrap();
-        // }
-        // _result.insert(record.get(0).unwrap().parse().unwrap(), record.get(1).unwrap().parse().unwrap());
 
-        let key = record.get(0).unwrap().parse().unwrap();
-        let value = f64::from_str(&record.get(1).unwrap()).unwrap();
+        let tenancy = record.get(0).unwrap().parse().unwrap();
+        let prob = f64::from_str(&record.get(1).unwrap()).unwrap();
 
-        if key > largest {
-            largest = key;
+        if tenancy > largest {
+            largest = tenancy;
         }
-        _result.insert(key, value);
+        tenancy_dist.insert(tenancy, prob);
     }
-    return (_result, largest as usize);
+    return (Generator::new(tenancy_dist.into_iter()), largest as usize);
 }
 
 
@@ -155,16 +128,18 @@ fn write(output: Vec<u64>) {
     let mut wtr = csv::Writer::from_writer(io::stdout());
     let mut index: usize = 0;
     wtr.write_record(&["DCS", "probability"]).expect("cannot write");
-    for key in output {
+    output.iter().for_each(|&key| {
         wtr.write_record(&[index.to_string(), ((key as f64) / sum as f64).to_string()]).expect("cannot write");
+        // println!("{}: {}", index, key);
         index += 1;
-    }
+    });
     println!("sum: {}", sum);
 }
 
 
 fn main() {
-    let test = input_to_hashmap();
-    let test_1 = caching(Generator::new(test.0.into_iter()), 10, 0.005, test.1);
-    write(test_1);
+    // let test = input_to_hashmap();
+    let (tenancy_dist, largest) = input_to_hashmap();
+    let output = caching(tenancy_dist, 100, 0.005, largest);
+    write(output);
 }
